14ff0d7c1ce2cf0fb0b29721e10284e01875926e
kaxxxrl <totalrthroandemomusics@gmail.com>
2025-04-30 13:30:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -460,4 +460,3 @@ if (message.content.toLowerCase().includes('wstawi') || message.content.toLowerC
 const now = Date.now();
 partnershipTimestamps.set(message.author.id, now);
 partneringUsers.delete(message.author.id);
-client.login(process.env.DISCORD_TOKEN);
